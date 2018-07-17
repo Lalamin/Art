b3fe138407c67eb5c89c3c95cd7507085b4dcc3f
Maria <msawaby@gmail.com>
2018-07-16 17:11:45 -0700

    first commit

---
+++ js/logic.js
@@ -0,0 +1,26 @@
+// $( window ).on( "load", changeComponent );
+//   function changeComponent(_src){
+// 		alert(_src);
+// 		console.log(JSON.stringify(_src));
+// 		$("#myImg").attr('src', _src);
+// 	}
+
+    // $("#bio").on("click", function() {
+
+    //     $("#toBRemove").load("Profile.htm");
+
+    // });
+// $(window).on('load', loadHome);
+
+// function loadProfile() {
+//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%" type="text/html" data="./Profile.htm" ></object>';
+//  document.getElementById("toBRemove").style.padding = "0px";
+// }
+// function loadHome() {
+//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px; overflow: hidden;" width="100%" height="100%" type="text/html" data="./Home.htm" ></object>';
+//  //document.getElementById("toBRemove").style.padding = "0px";
+// }
+// function loadExhibition() {
+//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%" type="text/html" data="./Exhibition.htm" ></object>';
+//  document.getElementById("toBRemove").style.padding = "0px";
+// }
