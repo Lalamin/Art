9ec7e49db5181e90cbcedcd3daf8d8d129b7eb4b
Maria <msawaby@gmail.com>
2018-07-20 22:07:46 -0700

    adding small images and changing some css

--- js/logic.js
+++ js/logic.js
@@ -12,15 +12,31 @@
     // });
 // $(window).on('load', loadHome);
 
-// function loadProfile() {
-//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%" type="text/html" data="./Profile.htm" ></object>';
-//  document.getElementById("toBRemove").style.padding = "0px";
-// }
+function loadProfile() {
+    if(document.getElementById("toBRemove")){
+        document.getElementById("toBRemove").style.height = "500px;";
+        document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%;" type="text/html" data="./Profile.htm" ></object>';
+        document.getElementById("toBRemove").style.padding = "0px";
+    }
+    else{
+        document.getElementById("toRemove").style.height = "500px;";
+        document.getElementById("toRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%;" type="text/html" data="./Profile.htm" ></object>';
+        document.getElementById("toRemove").style.padding = "0px";
+    }
+ 
+}
 // function loadHome() {
-//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px; overflow: hidden;" width="100%" height="100%" type="text/html" data="./Home.htm" ></object>';
+//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px; overflow: hidden;" width="130%" height="100%" type="text/html" data="./Home.htm" ></object>';
 //  //document.getElementById("toBRemove").style.padding = "0px";
 // }
-// function loadExhibition() {
-//  document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="100%" type="text/html" data="./Exhibition.htm" ></object>';
-//  document.getElementById("toBRemove").style.padding = "0px";
-// }
+function loadExhibition() {
+     if(document.getElementById("toBRemove")){
+         document.getElementById("toBRemove").style.height = "500px;";
+        document.getElementById("toBRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="130%" type="text/html" data="./Exhibition.htm" ></object>';
+        document.getElementById("toBRemove").style.padding = "0px";
+   } else{
+       document.getElementById("toRemove").style.height = "500px;";
+        document.getElementById("toRemove").innerHTML='<object style="margin: 0px 0px" width="100%" height="130%" type="text/html" data="./Exhibition.htm" ></object>';
+        document.getElementById("toRemove").style.padding = "0px";
+    }
+}
